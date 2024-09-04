f7346fd6727ac985777795c4b8fc28dd2768f0b3
tcyang1971 <tcyang@gm.pu.edu.tw>
2024-09-04 14:35:35 +0800

    v1_文字元件

--- app/src/main/java/tw/edu/pu/csim/tcyang/oop/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/oop/MainActivity.kt
@@ -1,12 +1,16 @@
 package tw.edu.pu.csim.tcyang.oop
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var txv: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +20,7 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        txv = findViewById(R.id.txv)
+        txv.text = "物件導向實例"
     }
 }
